15842df403136cf3e43b8fab6a004f4f04beafb9
Dulaj Hashmika <dulajhashmika075@gmail.com>
2025-01-15 23:43:39 +0530

    create signup page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login.jsx";
+import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<SignUp/>} />
     </Routes>
   );
 }
